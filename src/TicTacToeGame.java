40b01e0bd3a4a75ca76ef825f77f7055aa9bbdc2
Avani14 <trivediavani14@gmail.com>
2022-05-30 20:42:48 +0530

    Ability to allow the

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
 
 public class TicTacToeGame {
 	public static char[] board = new char[] {'0','0','0','0','0','0','0','0','0','0'};
-
+	public static String userChar;
+	
+	public static void selection()
+	{
+		System.out.println("Enter X or O to select your character");
+		Scanner sc = new Scanner(System.in);
+		userChar = sc.next();
+		if(userChar.equals("X"))
+		{
+			System.out.println("Your character is X");
+		}
+		else if(userChar.equals("O"))
+		{
+			System.out.println("Your character is O");
+		}
+		else {
+			System.out.println("Please enter only X or O");
+		}
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("---Welcom to TicTacToeGame!!! -----");
+		selection();
 	}
 
 }
