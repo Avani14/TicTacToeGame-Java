71275f0a33578b1922e934f855c17c48bbd2ea25
Avani14 <trivediavani14@gmail.com>
2022-05-30 20:35:43 +0530

    As a Player would like to

---
+++ src/TicTacToeGame.java
@@ -0,0 +1,11 @@
+
+public class TicTacToeGame {
+	public static char[] board = new char[] {'0','0','0','0','0','0','0','0','0','0'};
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("---Welcom to TicTacToeGame!!! -----");
+	}
+
+}
